14827ef866c797efe63f3c2a230f9b07fca93396
Antonio <amatic@student.foi.hr>
2022-11-26 18:02:17 +0100

    EFLUSH-42 Implemented table admin_options creation through migrations.

--- api/src/db/migrations/1661970526687-ddl.ts
+++ api/src/db/migrations/1661970526687-ddl.ts
@@ -27,6 +27,19 @@ export class ddl1661970526687 implements MigrationInterface {
                 CONSTRAINT "username_unique" UNIQUE (username),
                 CONSTRAINT "email_unique" UNIQUE (email)
             );
+            DROP TABLE IF EXISTS "admin_options";
+            CREATE TABLE "admin_options" (
+                "id" serial NOT NULL,
+                "created_at" timestamp NOT NULL DEFAULT now(),
+                "updated_at" timestamp NOT NULL DEFAULT now(),
+                "deleted_at" timestamp,
+                "version" int NOT NULL,
+                "contact_email" varchar(45) NOT NULL,
+                "contact_phone" varchar(45) NOT NULL,
+                "currency" varchar(20) NOT NULL,
+                "capture_mode" varchar(45) NOT NULL,
+                PRIMARY KEY ("id")
+            );
             `
         );
     }
