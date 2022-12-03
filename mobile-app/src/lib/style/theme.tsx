29a599e55f6d4740763afdcf96a7d7722185c7f8
fsabolic <fsabolic@student.foi.hr>
2022-12-03 11:49:56 +0100

    EFLUSH-35 Added theme.tsx

--- mobile-app/src/lib/style/theme.tsx
+++ mobile-app/src/lib/style/theme.tsx
@@ -1,5 +1,12 @@
-export const fonts = {};
+export const font={
+    mainFont: '"Open Sans", sans-serif'
+}
 
-export const colors = {};
-
-export const breakpoints = {};
+export const color={
+    primaryBlue: '#006daa',
+    primaryOrange: '#ff7d00',
+    white: '#ffffff',
+    black: '#000000',
+    successGreen:'#006428',
+    failureRed:'#ad0000',
+}
