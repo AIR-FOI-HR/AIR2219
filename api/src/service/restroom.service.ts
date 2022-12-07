0e2bf5f0023bdc9d99557712c83200fd662cad37
Tomislav Fulir <tomislav.fulir@s2is.hr>
2022-12-07 22:58:51 +0100

    EFLUSH-46 Adeed error handling for uuid path param

--- api/src/service/restroom.service.ts
+++ api/src/service/restroom.service.ts
@@ -2,5 +2,8 @@ import { Restroom } from '../model/entity/Restroom';
 import { RestroomRepository } from '../dao/restroom.repository';
 
 export const getRestroomById = async (id: string): Promise<Restroom | null> => {
-  return RestroomRepository.findOne({where: {id}, relations: {city: true}});
+    if(id.length !== 36 || id.split(",").length - 1 !== 4) {
+      return null;
+    }
+    return RestroomRepository.findOne({where: {id}, relations: {city: true}});
 };
