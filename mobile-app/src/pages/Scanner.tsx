ff5de3cce7e88305253b110aae919e7a8bcd79be
Ivan Žuvela <jovis1796@gmail.com>
2022-12-07 16:11:12 +0100

    EFLUSH-38 Added base QR code scanner

--- mobile-app/src/pages/Scanner.tsx
+++ mobile-app/src/pages/Scanner.tsx
@@ -1,11 +1,46 @@
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
 
 const ScannerScreen: React.FC = () => {
-    return (
-      <View style={styles.container}>
-        <Text>Scanner</Text>
-      </View>
-    )
+  const [hasPermission, setHasPermission] = useState(false);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  interface Props {
+    type: string,
+    data: string
+  }
+
+  const handleBarCodeScanned = ({ type, data } : Props) => {
+    setScanned(true);
+    alert(`Bar code with type ${type} and data ${data} has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned && <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />}
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
