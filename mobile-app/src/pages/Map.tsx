aec5703b02defc6a2ff4f2e4b564a6dc188a2054
Ivan Žuvela <jovis1796@gmail.com>
2022-12-14 16:26:25 +0100

    EFLUSH-52 Implemented Google Maps

--- mobile-app/src/pages/Map.tsx
+++ mobile-app/src/pages/Map.tsx
@@ -1,20 +1,42 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from "react-native";
+import MapView from "react-native-maps";
 
-const MapScreen: React.FC = () => {
-    return (
-      <View style={styles.container}>
-        <Text>Map</Text>
-      </View>
-    )
+const varazdinRegion = {
+  latitude: 46.30774076067861,
+  longitude: 16.338090229632893,
+  latitudeDelta: 0.0412,
+  longitudeDelta: 0.0211,
+};
+
+const zagrebRegion = {
+  latitude: 45.8129526993897, 
+  longitude: 15.977320885433711,
+  latitudeDelta: 0.0412,
+  longitudeDelta: 0.0211,
 }
 
+const MapScreen: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <MapView
+        style={styles.map}
+        initialRegion={zagrebRegion}
+      />
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
 });
 
 export default MapScreen;
