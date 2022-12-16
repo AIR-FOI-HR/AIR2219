ace7e542f0c3e96c982f708b1cc47234b18498e0
Antonio <amatic@student.foi.hr>
2022-12-16 22:06:21 +0100

    EFLUSH-59 Implemented basic functionality for order.controller.ts

--- api/src/controller/order.controller.ts
+++ api/src/controller/order.controller.ts
@@ -1,10 +1,20 @@
-import express, { NextFunction, Request, Response } from 'express';
-import { validationResult } from 'express-validator';
-import { check } from 'express-validator';
+import express from 'express';
 import * as orderService from '../service/order.service';
 import * as dotenv from 'dotenv';
+import { Order } from '../model/entity/Order';
+import { AppError } from '../model/constants/AppError';
+import { OrderResponse } from '../model/response/OrderResponse';
 
 
 dotenv.config();
 
 const router = express.Router();
+
+router.get('/:userId', async (req, res, next) => {
+    const orders: Order[] | null = await orderService.getOrdersByUserId(req.params.userId);
+    if (!orders){
+        return next(new AppError("Orders not found for this user!", 404));
+    }
+    res.json(OrderResponse.toDtos(orders));
+
+});
