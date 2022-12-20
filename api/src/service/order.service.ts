79dce95233cf46187fa53f2a4f56860fbe317d34
Antonio <amatic@student.foi.hr>
2022-12-20 11:12:59 +0100

    EFLUSH-59 Implemented sorting of orders by chosen sort field, field being createdAt, updatedAt or deletedAt

--- api/src/service/order.service.ts
+++ api/src/service/order.service.ts
@@ -2,7 +2,7 @@ import { Order } from '../model/entity/Order';
 import { OrderRepository } from '../dao/order.repository';
 import { FindOptionsOrderValue } from 'typeorm';
 
-export const getOrdersByUserId = async (userId: string, cityId?: string, sortDirection: string = "DESC"): Promise<Order[] | null> => {
+export const getOrdersByUserId = async (userId: string, cityId?: string, sortDirection: string = "DESC", sortField: string = "createdAt"): Promise<Order[] | null> => {
   if(userId.length !== 36 || userId.split("-").length - 1 !== 4) {
     return null;
   }
@@ -11,10 +11,18 @@ export const getOrdersByUserId = async (userId: string, cityId?: string, sortDir
 
   if (!cityId){
     orders = OrderRepository.find({relations: {restroom: {city: true}, amount: true}, where: {user: { id: userId }}});
-  } else if (cityId){
+  } else if (cityId && sortField == "createdAt"){
     orders = OrderRepository.find({relations: {restroom: {city: true}, amount: true}, 
       where: {user: { id: userId }, restroom: {city: {id: cityId}}}, 
       order: {createdAt: sortDirection as FindOptionsOrderValue}});
+  } else if (cityId && sortField == "updatedAt"){
+    orders = OrderRepository.find({relations: {restroom: {city: true}, amount: true}, 
+      where: {user: { id: userId }, restroom: {city: {id: cityId}}}, 
+      order: {updatedAt: sortDirection as FindOptionsOrderValue}});
+  } else if (cityId && sortField == "deletedAt"){
+    orders = OrderRepository.find({relations: {restroom: {city: true}, amount: true}, 
+      where: {user: { id: userId }, restroom: {city: {id: cityId}}}, 
+      order: {deletedAt: sortDirection as FindOptionsOrderValue}});
   }
   
   return orders;
