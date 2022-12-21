0344d10baf3f46397f7e131ff3b4f4f49ab6d908
Antonio <amatic@student.foi.hr>
2022-12-21 11:46:28 +0100

    EFLUSH-59 Changed the OrderResponse so it uses RestroomResponse as value for its restroom attribute.

--- api/src/model/response/OrderResponse.ts
+++ api/src/model/response/OrderResponse.ts
@@ -1,13 +1,13 @@
-import { Restroom } from "../entity/Restroom";
 import { Order } from "../entity/Order"
 import { Amount } from "../entity/Amount";
 import { OrderState } from "../constants/OrderState";
+import { RestroomResponse } from "./RestroomResponse";
 
 export class OrderResponse{
     constructor(
         public orderState: OrderState,
         public amount: Amount,
-        public restroom: Restroom
+        public restroom: RestroomResponse
     ) {}
 
     
@@ -15,15 +15,13 @@ export class OrderResponse{
         return new OrderResponse(
             order.state,
             order.amount,
-            order.restroom   
+            RestroomResponse.toDto(order.restroom) 
         );
     }
 
     public static toDtos(orders: Order[]): OrderResponse[] {
         let orderResponses: OrderResponse[] = [];
         orders.forEach((order: Order) => {
-            console.log(OrderResponse.toDto(order));
-
             orderResponses.push(OrderResponse.toDto(order))
         });
         return orderResponses;
