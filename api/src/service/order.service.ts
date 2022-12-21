8fbdfa2ae86c33c811b3873f120fcb78acf247ac
Antonio <amatic@student.foi.hr>
2022-12-21 11:44:56 +0100

    EFLUSH-59 Fixed implementation to be cleaner and more intuitive and flexible with added values dependable on parameter values.

--- api/src/service/order.service.ts
+++ api/src/service/order.service.ts
@@ -1,29 +1,26 @@
 import { Order } from '../model/entity/Order';
 import { OrderRepository } from '../dao/order.repository';
-import { Between, FindOptionsOrderValue } from 'typeorm';
+import { Between, Like } from 'typeorm';
 
-export const getOrdersByUserId = async (userId: string, dateFrom: Date, dateTo: Date, cityId?: string, sortDirection: string = "DESC", sortField: string = "createdAt"): Promise<Order[] | null> => {
+export const getOrdersByUserId = async (userId: string, dateFrom: Date, dateTo: Date, cityId: string, sortDirection: string, sortField: string): Promise<Order[] | null> => {
   if(userId.length !== 36 || userId.split("-").length - 1 !== 4) {
     return null;
   }
 
-  let orders = null;
-
-  if (!cityId){
-    orders = OrderRepository.find({relations: {restroom: {city: true}, amount: true}, where: {user: { id: userId }}});
-  } else if (cityId && sortField == "createdAt"){
-    orders = OrderRepository.find({relations: {restroom: {city: true}, amount: true}, 
-      where: {user: { id: userId }, restroom: {city: {id: cityId}}, createdAt: Between(dateFrom, dateTo)}, 
-      order: {createdAt: sortDirection as FindOptionsOrderValue}});
-  } else if (cityId && sortField == "updatedAt"){
-    orders = OrderRepository.find({relations: {restroom: {city: true}, amount: true}, 
-      where: {user: { id: userId }, restroom: {city: {id: cityId}}, updatedAt: Between(dateFrom, dateTo)}, 
-      order: {updatedAt: sortDirection as FindOptionsOrderValue}});
-  } else if (cityId && sortField == "deletedAt"){
-    orders = OrderRepository.find({relations: {restroom: {city: true}, amount: true}, 
-      where: {user: { id: userId }, restroom: {city: {id: cityId}}, deletedAt: Between(dateFrom, dateTo)}, 
-      order: {deletedAt: sortDirection as FindOptionsOrderValue}});
+  console.log(cityId);
+  if(cityId){
+  if( cityId.length !== 36 || cityId.split("-").length - 1 !== 4 ) {
+    return null;
+  }
+  } else if (cityId == "") {
+    return null;
   }
   
+  let orders = null;
+
+  orders = await OrderRepository.find({relations: {restroom: {city: true}, amount: true}, 
+    where: {user: { id: userId}, restroom: {city: {id: cityId}}, createdAt: Between(dateFrom, dateTo)}, 
+    
+    order: {[sortField]: sortDirection}});
   return orders;
 };
