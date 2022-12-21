976f343f49001726ae6df945402b8a7c4689488c
fsabolic <fsabolic@student.foi.hr>
2022-12-21 14:58:25 +0100

    EFLUSH-60 Started creating PurchaseHistory page

---
+++ mobile-app/src/pages/PurchaseHistory.tsx
@@ -0,0 +1,26 @@
+import { FlatList, StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+import PastOrder from '../components/PastOrder'
+
+
+
+
+
+const PurchaseHistory : React.FC = () => {
+
+    type OrderProba = {
+        date:string,
+        state:boolean,
+        price: number,
+    }
+
+    return (
+        <FlatList>
+            
+        </FlatList>
+    )
+}
+
+export default PurchaseHistory
+
+const styles = StyleSheet.create({})
--- mobile-app/src/pages/UserProfile.tsx
+++ mobile-app/src/pages/UserProfile.tsx
@@ -1,21 +1,58 @@
-import { StyleSheet, Text, View, Image } from "react-native";
-import IconPointer from "../assets/ic_CurrentLocation.svg";
+import { StyleSheet, Text, View, Button } from "react-native";
+import Logo from '../assets/ic_Logo.svg';
+import { color } from "../lib/style/theme";
+import TitleCurve from '../assets/vec_TitleCurve.svg';
+import PurchaseHistory from "./PurchaseHistory";
+import PastOrder from "../components/PastOrder";
+import { Restroom } from "../api/models/response/Restroom";
 
-const UserProfileScreen = () => {
+
+const UserProfileScreen : React.FC = ({navigation}) => {
   return (
+    // <View style={styles.container}>
+    //   <View style={styles.itemContainer}>
+    //     <Logo width={150} height={150}/>
+    //     <Text>-- TO DO -- T I T L E</Text>
+    //     <Text>-- TO DO -- V E R S I O N</Text>
+
+    //     <View style={styles.titleCurve}><TitleCurve/></View>
+
+    //   </View>
+
+    //   <View>
+    //     <Button title='Past purchases' onPress={navigation.navigate('')}/>
+    //   </View>
+      
+    // </View>
     <View style={styles.container}>
-      <Text>UserProfile</Text>
+      <Button title={'asdf'} onPress= {()=>{navigation.navigate('purchaseHistory')}}/>
     </View>
+
   );
 };
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: color.white,
+  //   alignItems: "center",
+  //   justifyContent: "flex-start",
+  //   padding:30,
+  //   height:'100%',
+  // },
+  // itemContainer:{
+  //   alignItems: "center",
+  //   justifyContent: 'space-between',
+  // },
+  // titleCurve:{
+  //   margin:40,
+  // }
+
+  container:{
+    alignItems:'center',
+    justifyContent:'center',
+    flex:1,
+  }
 });
 
 export default UserProfileScreen;
