986f0f69b0d20bdb5d9b253bb7c3da6e0fe98142
Antonio <amatic@student.foi.hr>
2023-01-04 12:42:02 +0100

    EFLUSH-68 Added the functionality of verifying the jwt token given including extraction of the jwt token payload and mapping these values to request for actual endpoint. These values shall be updated when the actual jwt token creation is implemented with login functionality. For testing purposes, I wrote a .ts file that will create and sign me a test jwt and using given jwt and secret key = test the authentication middleware works. The .ts file for test jwt creation is not provided. JWT will eb provided in next commit

--- api/src/middleware/auth.ts
+++ api/src/middleware/auth.ts
@@ -1,14 +1,19 @@
+const jwt = require("jsonwebtoken")
 import { NextFunction } from "express";
 
 
 export async function authenticateRequest( req: Request, _res: Response, next: NextFunction ){
     try {
+
         //Authorization: Bearer a2f23...
-        const token = req.headers.authorization;
+        const token = req.headers.authorization.split(" ")[1];
         
         if (!token) {
-            throw new Error("");
+            throw new Error("Authentication failed.");
         }
+
+        const payload = jwt.verify(token, process.env.JWT_KEY);
+        req.userData = { id: payload.username, isAdmin: payload.isAdmin };
 
         next();
         
