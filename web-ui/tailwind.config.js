b8c9c9cf72fe86586490d0b68296cf7b4b64fdad
fsabolic <fsabolic@student.foi.hr>
2023-01-05 02:28:12 +0100

    EFLUSH-76 Added colors and fonts to tailwind.config.js and theme.tsx

--- web-ui/tailwind.config.js
+++ web-ui/tailwind.config.js
@@ -1,10 +1,26 @@
 /** @type {import('tailwindcss').Config} */
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primaryBlue: 'rgba(0,109,170,1)',
+        primaryOrange: 'rgba(255,125,0,1)',
+        white: 'rgba(255,255,255,1)',
+        black: 'rgba(0,0,0,1)',
+        successGreen:'rgba(0,100,40,1)',
+        failureRed:'rgba(173,0,0,1)',
+        transparentGray: 'rgba(0,0,0,0.5)',
+        borderGrey: "rgba(189,189,189,1)"
+      },
+      fontFamily:{
+        openSans:['Open Sans','cursive'],
+      }
+
+    },
   },
   plugins: [],
 }
